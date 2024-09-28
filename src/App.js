d92b974f4ff24b7c33a102ec4ec0739a319395e1
sushantbro12 <sushantbro12@gmail.com>
2024-09-28 17:43:06 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ProductCard from "./Components/ProductCard";
+import useFetch from "./Components/useFetch";
 
 function App() {
+  const [products, loading, error] = useFetch();
+  const [favourite, setFavourite] = useState([]);
+
+  //loading fav when App comp is created
+  useEffect(() => {
+    const storedFavourites = JSON.parse(localStorage.getItem("favourites"));
+    setFavourite(storedFavourites);
+  }, []);
+
+  //handling adding and removing fav
+
+  const handleFavourite = (
+    id,
+    title,
+    image,
+    description,
+    price,
+    isFavourite
+  ) => {
+    let updatedFavourites;
+
+    if (isFavourite) {
+      //adding to fav
+      const newFavourite = { id, title, image, description, price }; //new object
+      updatedFavourites = [...favourite, newFavourite];
+    } else {
+      //removing from fav
+      updatedFavourites = favourite.filter((fav) => fav.title !== title);
+    }
+
+    setFavourite(updatedFavourites);
+    localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <p>loading.....</p>}
+      {error && <p>Error occured while fetching products.</p>}
+      {/* <span>{JSON.stringify(products)}</span> */}
+      {/* {console.log(products)} */}
+
+      <h2>All Products</h2>
+      {products.map((product) => {
+        return (
+          <ProductCard
+            key={product.id}
+            title={product.title}
+            image={product.image}
+            description={product.description}
+            price={product.price}
+            isFavourite={favourite.some((fav) => fav.title === product.title)}
+            onHandleFavourite={handleFavourite}
+          />
+        );
+      })}
+
+      <h2>My favourites</h2>
+      <div>
+        {favourite.length > 0 ? (
+          favourite.map((favourite) => (
+            <ProductCard
+              key={favourite.id}
+              title={favourite.title}
+              image={favourite.image}
+              description={favourite.description}
+              price={favourite.price}
+              isFavourite={true}
+              onHandleFavourite={handleFavourite}
+            />
+          ))
+        ) : (
+          <p>No current favourites</p>
+        )}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// id:1,
+// title:'...',
+// price:'...',
+// category:'...',
+// description:'...',
+// image:'...'
---
+++ src/Components/ProductCard.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import "./product.css";
+
+const ProductCard = ({
+  key,
+  title,
+  image,
+  description,
+  price,
+  isFavourite,
+  onHandleFavourite,
+}) => {
+  const [isFav, setIsFav] = useState(isFavourite);
+
+  const handleFavourite = () => {
+    const newFavState = !isFav;
+    setIsFav(newFavState);
+    onHandleFavourite(key, title, image, description, price, newFavState);
+  };
+
+  useEffect(() => {
+    setIsFav(isFavourite);
+  }, [isFavourite]);
+
+  return (
+    <div className="container">
+      <div className="card">
+        <img src={image} alt={title} className="card-image" />
+        <h1>{title}</h1>
+        <p className="price">{price}</p>
+        <p>{description}</p>
+        <p>
+          <button
+            onClick={handleFavourite}
+            style={{ background: isFav ? "red" : "black" }}>
+            {isFav ? "Remove from Favourite" : "Add to Favourite"}
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
